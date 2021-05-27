7f6c85f8902606f1d2305cdba98584b9e01f67e0
Jardel Mitermayer <jardel.filho@dcomp.ufs.br>
2021-05-27 02:25:56 -0300

    feat:redirecionando usuario apos login

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/produtos', 'App\Http\Controllers\ProdutoControlador@index');
+Route::get('/produtos', 'App\Http\Controllers\ProdutoControlador@index')->name('produtos');
